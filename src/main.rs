747dcbdba0df4420f16585ad94c67980ed9ea3b7
Mikael Souza <mikael.souza.cc@gmail.com>
2025-02-16 02:01:29 -0300

    adding second handler to test fly.io auto deploy

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,15 @@ use axum::{response::Html, routing::get, Router};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(handler));
-
+    let app = Router::new().route("/", get(handler)).route("/mikas", get(handler2));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
 async fn handler() -> Html<&'static str> {
     Html("<h1>Hello, World!</h1>")
+}
+
+async fn handler2() -> Html<&'static str> {
+    Html("<h1>Hello, Miskas!</h1>")
 }
